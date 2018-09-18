fbbd2a379f2384189e9e64ad3e90a0435bc60052
Robert Chatley <robert@develogical.com>
2018-09-18 12:15:29 +0100

    try breaking the build, just to check

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -8,6 +8,6 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
-        return "";
+        return "NO RESULTS";
     }
 }
