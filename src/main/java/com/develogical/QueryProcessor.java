4f08a4f0715881cecc9cdd5f84c6eb6e1d0312f2
Hubert Kaluzny <contact@hubertkaluzny.com>
2021-01-23 10:11:50 +0000

    Add new test for Turing

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -8,6 +8,13 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist. " +
                     "Shakespeare produced most of his known works between 1589 and 1613.";
+        } else if (query.toLowerCase().contains("turing")) {
+            return "Alan Mathison Turing was an English mathematician, computer scientist, " +
+                    "logician, cryptanalyst, philosopher, and theoretical biologist. " +
+                    "Turing was highly influential in the development of theoretical " +
+                    "computer science, providing a formalisation of the concepts of " +
+                    "algorithm and computation with the Turing machine, which can be " +
+                    "considered a model of a general-purpose computer.";
         }
         return "";
     }
--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -24,4 +24,9 @@ public class QueryProcessorTest {
     public void isNotCaseSensitive() throws Exception {
         assertThat(queryProcessor.process("shakespeare"), containsString("playwright"));
     }
+
+    @Test
+    public void knowsAboutTuring() throws Exception {
+        assertThat(queryProcessor.process("Turing"), containsString("computer scientist"));
+    }
 }
