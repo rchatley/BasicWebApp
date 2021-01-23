b4a3f455523ce8a69825dfcaa35fb3520dba4079
Hubert Kaluzny <contact@hubertkaluzny.com>
2021-01-23 10:14:55 +0000

    Misspell turing to test CI pipeline

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -8,7 +8,7 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist. " +
                     "Shakespeare produced most of his known works between 1589 and 1613.";
-        } else if (query.toLowerCase().contains("turing")) {
+        } else if (query.toLowerCase().contains("turing_")) {
             return "Alan Mathison Turing was an English mathematician, computer scientist, " +
                     "logician, cryptanalyst, philosopher, and theoretical biologist. " +
                     "Turing was highly influential in the development of theoretical " +
