6486a1fd377ffa350c87466604f7536198635e9f
jackmorrison <jsm1317@ic.ac.uk>
2021-01-26 15:52:52 +0000

    Updates messaging on index page

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -8,7 +8,7 @@ public class IndexPage extends HtmlPage {
     protected void writeContentTo(PrintWriter writer) {
         writer.println(
                 "<h1>Welcome!</h1>" +
-                "<p>Enter your query in the box below: " +
+                "<p>Please enter your query in the box below: " +
                   "<form><input type=\"text\" name=\"q\" />" +
                     "<input type=\"submit\">" +
                   "</form>" +
