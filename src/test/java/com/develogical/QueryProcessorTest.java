2b2faf2b7a5769e2cb6e747ef31413a86b6902dc
Robert Chatley <robert@develogical.com>
2021-12-19 11:52:52 +0000

    Fix test and import

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class QueryProcessorTest {
 
@@ -17,7 +17,7 @@ public class QueryProcessorTest {
 
     @Test
     public void knowsAboutShakespeare() throws Exception {
-        assertThat(queryProcessor.process("Shakespeare"), containsString("goldfinch"));
+        assertThat(queryProcessor.process("Shakespeare"), containsString("playwright"));
     }
 
     @Test
