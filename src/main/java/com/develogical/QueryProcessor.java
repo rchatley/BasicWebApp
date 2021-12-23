54d88a0885381737bfca12c2fff9b7e74daae436
Robert Chatley <robert@develogical.com>
2021-12-23 13:12:08 +0000

    Back to default state.

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -8,9 +8,6 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
-        if (query.toLowerCase().contains("name")) {
-          return "Test Player";
-        }
         return "";
     }
 }
