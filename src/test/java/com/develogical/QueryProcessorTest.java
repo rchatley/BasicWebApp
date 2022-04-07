6078c8dbedc00dd84a6011472148e7a35afaeca1
Robert Chatley <robert@develogical.com>
2022-04-07 14:27:28 +0100

    Answer team name.

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -24,4 +24,9 @@ public class QueryProcessorTest {
     public void isNotCaseSensitive() throws Exception {
         assertThat(queryProcessor.process("shakespeare"), containsString("playwright"));
     }
+
+    @Test
+    public void knowsTeamName() throws Exception {
+        assertThat(queryProcessor.process("what is your name"), containsString("MyTeam"));
+    }
 }
