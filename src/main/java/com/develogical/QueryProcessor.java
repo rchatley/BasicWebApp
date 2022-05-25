bf658f99739c0aefaa81ab5f9d1835ffe3b8a91d
Robert Chatley <rbc@imperial.ic.ac.uk>
2022-05-25 16:15:51 -0400

    Answer Team Name

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -8,6 +8,9 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+        if (query.toLowerCase().contains("team name")) {
+            return "MyTeam";
+        }
         return "";
     }
 }
