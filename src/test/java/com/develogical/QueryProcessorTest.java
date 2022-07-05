206978541ff140a14fe3df38a21d2c8b31f947ee
Robert Chatley <rbc@imperial.ic.ac.uk>
2022-07-05 10:37:09 +0200

    Answer Team name

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -25,4 +25,9 @@ public class QueryProcessorTest {
         assertThat(queryProcessor.process("shakespeare"), containsString("playwright"));
     }
 
+    @Test
+    public void knowsTeamNames() throws Exception {
+        assertThat(queryProcessor.process("what is your name"), containsString("Robert"));
+    }
+
 }
