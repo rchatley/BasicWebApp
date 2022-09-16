9068680f7650896390b45c2ce40b44b6e1bfbfe9
Robert Chatley <rbc@imperial.ic.ac.uk>
2022-09-16 14:59:02 +0100

    Revert "Answer Team name"
    This reverts commit 206978541ff140a14fe3df38a21d2c8b31f947ee.

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -7,8 +7,6 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } if (query.toLowerCase().contains("name")) {
-            return "Robert";
         }
         return "";
     }
